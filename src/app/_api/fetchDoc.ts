74cf9feeb4223d9ef80eaa0cd81c574e83bd7ee8
tutich <pkoinange02@gmail.com>
2024-06-26 02:01:10 +0300

    categories

--- src/app/_api/fetchDoc.ts
+++ src/app/_api/fetchDoc.ts
@@ -7,6 +7,7 @@ import { PRODUCT } from '../_graphql/products'
 import { GRAPHQL_API_URL } from './shared'
 import { payloadToken } from './token'
 
+
 const queryMap = {
   pages: {
     query: PAGE,
@@ -20,6 +21,7 @@ const queryMap = {
     query: ORDER,
     key: 'Orders',
   },
+ 
 }
 
 export const fetchDoc = async <T>(args: {
