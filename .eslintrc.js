20662e37be347d2840cbffb915557d9f1bb33904
tutich <pkoinange02@gmail.com>
2024-06-27 23:00:24 +0300

    plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms', 'plugin:prettier/recommended'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-}
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': 'error', // enforce prettier formatting
+    'simple-import-sort/imports': 'error', // enforce import sorting
+    'no-console': 'warn', // optionally warn about console statements
+  },
+};
