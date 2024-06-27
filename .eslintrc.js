1f6b0a6da591a56741cc65112128dfb1eed22b2c
tutich <pkoinange02@gmail.com>
2024-06-27 22:24:53 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
   plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': 'off', // Disable all prettier rules
-    'simple-import-sort/imports': 'error', // Enable simple import sort for imports
-    'simple-import-sort/exports': 'error', // Enable simple import sort for exports
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-console': 'off', // Disable no-console rule
+    'eol-last': ['error', 'always'], // Ensure newline at end of file
+    'comma-dangle': ['error', 'always-multiline'], // Ensure trailing commas
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Allow unused vars prefixed with _
   },
 };
