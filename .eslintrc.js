e24873b4ee378273fc92f8cb5f686818c885d005
tutich <pkoinange02@gmail.com>
2024-06-27 22:17:18 +0300

    import sorts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': 'off', // Disable all prettier rules
+    'simple-import-sort/imports': 'error', // Enable simple import sort for imports
+    'simple-import-sort/exports': 'error', // Enable simple import sort for exports
   },
 };
