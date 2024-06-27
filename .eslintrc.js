a1f35fa08d296c83ae85f8a7272cb890fb524d83
tutich <pkoinange02@gmail.com>
2024-06-27 22:49:42 +0300

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,5 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier', 'simple-import-sort'],
-  rules: {
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'no-console': 'warn', // Change to 'warn' if you want to keep console statements during development
-    'eol-last': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto', // Adjust based on your project's requirements
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 80,
-      },
-    ],
-  },
+  plugins: ['prettier'],
 }
