05ce89f76cc15f65d2bcc4eb37d95726e65c590a
tutich <pkoinange02@gmail.com>
2024-06-27 22:29:19 +0300

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,6 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier', 'simple-import-sort'],
-  rules: {
-    'prettier/prettier': 'off', // Disable all prettier rules
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'no-console': 'off', // Disable no-console rule
-    'eol-last': ['error', 'always'], // Ensure newline at end of file
-    'comma-dangle': ['error', 'always-multiline'], // Ensure trailing commas
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Allow unused vars prefixed with _
-  },
+  plugins: ['prettier' ],
+
 };
