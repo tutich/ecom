2ee69c0978dc00fa3120e6344a4beeb21557f083
tutich <pkoinange02@gmail.com>
2024-06-27 23:40:59 +0300

    mod eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': 0, // enforce prettier formatting
-    'simple-import-sort/imports': 'error', // enforce import sorting
-  
+    'simple-import-sort/imports': error, // enforce import sorting
+    'no-console': 'warn', // optionally warn about console statements
+    'eol-last': ['error', 'always'], // enforce newline at end of file
   },
 };
